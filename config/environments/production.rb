756c3b08b3e9f981a59ce5521eb3f2dd8aedd97b
soundsnick <soundsnick@gmail.com>
2019-07-19 22:33:49 +0600

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
       user_name:            'upmindkazakhstan@gmail.com',
       password:             'Asparagine12345',
       authentication:       'plain',
-      enable_starttls_auto: true
+      # enable_starttls_auto: true
   }
 
 
