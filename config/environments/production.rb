ec564c2dfdb3d2c8ebde8c2c481876a9ab79bd1d
soundsnick <soundsnick@gmail.com>
2019-07-19 22:48:38 +0600

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'upmind.kz',
+      domain:               'gmail.com',
       user_name:            'upmindkazakhstan@gmail.com',
       password:             'Asparagine12345',
       authentication:       'plain',
